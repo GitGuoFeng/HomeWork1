f05bd1ce05b3a2e4d823b28f2143bbccaba654b9
Your Name <email@example.com>
2022-04-28 10:45:42 +0800

    homework

--- app/src/main/java/com/example/homework/Main3Activity.java
+++ app/src/main/java/com/example/homework/Main3Activity.java
@@ -30,7 +30,6 @@ public class Main3Activity extends AppCompatActivity {
     private SimpleAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main3);
 
